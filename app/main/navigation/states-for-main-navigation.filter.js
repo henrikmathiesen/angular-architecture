781e7146bd6ada7050af2c0895792ef41cd09ac3
Henrik <henrik_mathiesen@hotmail.com>
2015-12-30 23:39:55 +0100

    -begun new filter

--- app/main/navigation/states-for-main-navigation.filter.js
+++ app/main/navigation/states-for-main-navigation.filter.js
@@ -6,19 +6,28 @@ angular
             // Remove the abstract state, at index 0, from the array
             states.shift();
             
-            // Remove states that does not have an URL
-            for (var i = 0; i < states.length; i++) {
-                if(!states[i].url) {
-                    states.splice([i], 1);
-                }
-            }
-            
-            // Remove states that is a sub state
-            for (var j = 0; j < states.length; j++) {
-                if(states[j].name.indexOf('.') > -1) {
-                    states.splice([j], 1);
-                }
-            }
+            // var _stateDoesNotHaveAnUrl = function(state){
+            //     return state.url !== true;
+            // };
+            // 
+            // var _stateIsThes404State = function(state){
+            //     return state.name === 's404';
+            // };
+            // 
+            // var _stateIsNestedState = function(state){
+            //     return state.name.indexOf('.') > -1;
+            // };
+            // 
+            // for (var i = 0; i < states.length; i++) {
+            //     console.log(i);
+            //     if(_stateDoesNotHaveAnUrl(states[i]) || _stateIsThes404State(states[i]) || _stateIsNestedState(states[i])) {
+            //         console.log("=== SPLICING ===");
+            //         console.log(states[i]);
+            //         console.log("=== /SPLICING ===");
+            //         states.splice([i], 1);
+            //         //continue;
+            //     }
+            // }
             
             return states;
         };
