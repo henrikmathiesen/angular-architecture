9db8f9646e0ed715458721a6c932e216db590db5
Henrik <henrik_mathiesen@hotmail.com>
2016-01-02 17:55:40 +0100

    -unit tests

--- tst/main.test.js
+++ tst/main.test.js
@@ -3,4 +3,15 @@ describe("Application should load its modules", function(){
     it("finds and load main module, which is dependant on existing dependencies also loads", function(){
         module('main'); // This requires ng-mock (bower component, included in karma config file list)
     });
+    
+});
+
+describe("main module", function(){
+    
+    //beforeEach
+    
+    it("should have a main controller with a title property on scope", function(){
+        
+    });
+    
 });
