ed77238a27471e7331111dba4e2f14c596a101b4
Henrik <henrik_mathiesen@hotmail.com>
2016-01-03 10:01:02 +0100

    -comments on controller as

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -14,4 +14,17 @@ angular
     - In views we can better see what controller a property belongs to
     - To remedy unpredecteble bindings to child/parent scopes
     - https://toddmotto.com/digging-into-angulars-controller-as-syntax/
+    
+    # 
+    main as mainCtrl
+    var mainCtrl = this;
+    mainCtrl.title = "Foo";
+    
+    = 
+    $scope.mainCtrl = this;
+    $scope.mainCtrl.title = "Foo";
+    
+    ------------------------------
+    'controller as' does not meen that we loose scope, it is just syntax sugar on top of it
+    
 */
