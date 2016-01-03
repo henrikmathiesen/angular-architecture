cfb9574f9985deebc6e853eedfb6276dc29b73ab
Henrik <henrik_mathiesen@hotmail.com>
2016-01-03 12:59:14 +0100

    -used underscrore syntax for inject in test -comments

--- tst/main.test.js
+++ tst/main.test.js
@@ -2,7 +2,12 @@ describe("Application should load its modules", function () {
 
     it("finds and load main module, which is dependant on existing dependencies also loads", function () {
         // This requires ng-mock (bower component, included in karma config file list)
-        // Short hand for angular.mock.module, https://docs.angularjs.org/api/ngMock/function/angular.mock.module
+        // Short hand for 'angular.mock.module' respective 'angular.mock.inject', https://docs.angularjs.org/api/ngMock/function/angular.mock.module
+        // 'module' = load our modules, or create one with an inline function
+        // 'inject' = get instances of components from modules, like factories
+        // use injected '$controller' to get hold of a controller, see bellow, or create a new one with inline function
+        // _injected_ is a convention to avoid naming conflicts
+        // 'dump' = dump logging, console.log(angular.mock.dump(someObject)); karma also has a dump function; dump(angular.mock.dump(someObject));
         module('main');
     });
 
@@ -15,8 +20,8 @@ describe("main module", function () {
     beforeEach(function () {
         module('main');
 
-        inject(function ($controller) {
-            ctrl = $controller('main');
+        inject(function (_$controller_) {
+            ctrl = _$controller_('main');
         });
     });
 
