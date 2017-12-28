ea8918a2ddd4321509e8d755c7cba639e20045c2
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-12-28 10:44:24 +0100

    -input mask directive skeletons and use on about page

--- app/shared/input-mask-dec/input-mask-dec.directive.js
+++ app/shared/input-mask-dec/input-mask-dec.directive.js
@@ -1,1 +1,12 @@
-// TODO
+angular
+    .module('main.shared')
+    .directive('aaInputMaskDec', function () {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'shared/input-mask-dec/input-mask-dec.template.html',
+            link: function (scope, $element) {
+                console.log('aaInputMaskDec');
+            }
+        };
+    });
--- app/shared/input-mask-int/input-mask-int.directive.js
+++ app/shared/input-mask-int/input-mask-int.directive.js
@@ -1,1 +1,13 @@
-// TODO
+angular
+    .module('main.shared')
+    .directive('aaInputMaskInt', function () {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'shared/input-mask-int/input-mask-int.template.html',
+            link: function (scope, $element) {
+                console.log('aaInputMaskInt');
+            }
+        };
+    });
+
