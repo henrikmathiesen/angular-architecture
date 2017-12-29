dbef631881cbe1cf76ce3739fc6412a113fc2cf3
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-12-29 11:30:09 +0100

    -comments

--- app/shared/input-mask-int/input-mask-int.directive.js
+++ app/shared/input-mask-int/input-mask-int.directive.js
@@ -1,9 +1,18 @@
-// Prio 1
+// Prio 1 (works, sort of, but glitchy)
 // https://www.npmjs.com/package/angular-input-masks
 // Requires: https://docs.angularjs.org/guide/i18n
 
-// Prio 2
+// Prio 2 (not good for currency: https://github.com/angular-ui/ui-mask/issues/92)
 // https://github.com/angular-ui/ui-mask
+
+// Prio 3 (not good for currency: https://github.com/candreoliveira/ngMask/issues/68)
+// https://github.com/candreoliveira/ngMask
+
+// Prio 4 (works, but only formats on blur)
+// https://github.com/aguirrel/ng-currency
+
+// Prio 5 (jQuery)
+// https://github.com/RobinHerbots/Inputmask
 
 angular
     .module('main.shared')
